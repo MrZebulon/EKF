e1a4bd21edac99f8cfe35aa67b66cb541572696e
BuildTools <unconfigured@null.spigotmc.org>
2023-04-09 15:20:03 +0200

    sensor noises

---
+++ sensors_noises.m
@@ -0,0 +1,14 @@
+accel_bias = 0;
+accel_noise = 7.05E-04;
+accel_bias_noise =  6.89e-4;
+
+% change units: currently in hPa
+baro_bias = 0; 
+baro_noise = 1.52e-5; 
+baro_bias_noise = 2.98e-7;
+baro_measurement_uncertainty = 0.1;
+
+additiveNoise = 1e-8;
+scale_var = -1;
+vel_delta_bias_sigma = -1;
+pos_delta_bias_sigma = -1;
