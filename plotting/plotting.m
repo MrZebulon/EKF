130246a03fa37ec228e8037b8e1d7345041e3c56
BuildTools <unconfigured@null.spigotmc.org>
2023-04-29 16:57:02 +0200

    Refactored plotting

--- plotting/plotting.m
+++ plotting/plotting.m
@@ -2,7 +2,7 @@ close all
 clc
 clear
 
-% Import
+%% Import
 data = load("./data/static_out.csv");
 %data = system_states;
 
@@ -13,7 +13,9 @@ pos = data(:, 1:3);
 vel = data(:, 4:6);
 ori = data(:, 7:10);
 
-% VELOCITY PLOT
+progressive = true;
+
+%% VELOCITY PLOT
 figure
 t = linspace(0, N * dt, N);
 
@@ -35,7 +37,7 @@ axis([min(t), max(t), min(vel(:, 3)), max(vel(:, 3))]);
 ylabel("v_z (m/s)")
 grid on
 
-% ORIENTATION PLOT
+%% ORIENTATION PLOT
 figure
 t = linspace(0, N * dt, N);
 [yaw, pitch, roll] = quat2angle(ori);
@@ -59,7 +61,7 @@ axis([min(t), max(t), min(roll(:)), max(roll(:))]);
 ylabel("Roll (rad)")
 grid on
 
-% POSITION PLOT
+%% POSITION PLOT
 figure
 
 point = scatter3(pos(1, 1),pos(1, 2),pos(1,3), 'red', 'diamond', 'LineWidth', 1);
@@ -75,22 +77,40 @@ zlabel("z (m)")
 
 grid on
 
-% RENDER
-for i =  1:N
-    set(vx, 'XData', t(1:i), 'YData', vel(1:i, 1))
-    set(vy, 'XData', t(1:i), 'YData', vel(1:i, 2))
-    set(vz, 'XData', t(1:i), 'YData', vel(1:i, 3))
+%% RENDER
+
+if progressive
+    for i =  1:N
+        set(vx, 'XData', t(1:i), 'YData', vel(1:i, 1))
+        set(vy, 'XData', t(1:i), 'YData', vel(1:i, 2))
+        set(vz, 'XData', t(1:i), 'YData', vel(1:i, 3))
+        drawnow limitrate
+    
+    
+        set(ay, 'XData', t(1:i), 'YData', yaw(1:i))
+        set(ap, 'XData', t(1:i), 'YData', pitch(1:i))
+        set(ar, 'XData', t(1:i), 'YData', roll(1:i))
+        drawnow limitrate
+    
+        set(trace,'Xdata',pos(1:i, 1),'Ydata',pos(1:i, 2),'Zdata',pos(1:i, 3));
+        hold on
+        set(point, 'Xdata',pos(i, 1),'Ydata',pos(i, 2),'Zdata',pos(i, 3))
+        drawnow limitrate
+    end
+else
+    set(vx, 'XData', t(1:N), 'YData', vel(1:N, 1))
+    set(vy, 'XData', t(1:N), 'YData', vel(1:N, 2))
+    set(vz, 'XData', t(1:N), 'YData', vel(1:N, 3))
     drawnow limitrate
 
 
-    set(ay, 'XData', t(1:i), 'YData', yaw(1:i))
-    set(ap, 'XData', t(1:i), 'YData', pitch(1:i))
-    set(ar, 'XData', t(1:i), 'YData', roll(1:i))
+    set(ay, 'XData', t(1:N), 'YData', yaw(1:N))
+    set(ap, 'XData', t(1:N), 'YData', pNtch(1:N))
+    set(ar, 'XData', t(1:N), 'YData', roll(1:N))
     drawnow limitrate
 
-    set(trace,'Xdata',pos(1:i, 1),'Ydata',pos(1:i, 2),'Zdata',pos(1:i, 3));
+    set(trace,'Xdata',pos(1:N, 1),'Ydata',pos(1:N, 2),'Zdata',pos(1:N, 3));
     hold on
-    set(point, 'Xdata',pos(i, 1),'Ydata',pos(i, 2),'Zdata',pos(i, 3))
+    set(point, 'Xdata',pos(N, 1),'Ydata',pos(N, 2),'Zdata',pos(N, 3))
     drawnow limitrate
-
 end
